861aaaeb0f887ee507b2a0e179a65c69f2afa1e7
Angel O. Lopez <32178007+AngelLopez8@users.noreply.github.com>
2019-12-04 16:57:20 -0600

    Add files via upload

--- MLT-receiver/MLT-receiver.ino
+++ MLT-receiver/MLT-receiver.ino
@@ -1,5 +1,9 @@
+#include <Pixy2.h>
 
- #include <Pixy2.h>
+/****mychanges******/
+bool bits[8];
+int bcount = 0;
+/******mychanges***/
 
 int red_sig = 1;
 int green_sig = 2;
@@ -7,6 +11,19 @@ Pixy2 pixy;
 
 int current = -1;
 int count = 0;
+
+/*****mychanges******/
+void trans(bool bits[]){
+  char ch;
+  int num = 0;
+  int base = 2;
+  for(int i = 7; i >= 0; i--){
+    num = num + (bits[i] * (pow(base, (7-i))));
+  }
+  ch = num;
+  Serial.print(ch);
+}
+/*******mychanges********/
 
 void setup(){
 
@@ -48,10 +65,17 @@ void loop(){
         current = -1;
         count = 0;
     }
-
+    /******my changes*****/
     if(count >= 60){
-      
-        Serial.print(current);  
+        bits[bcount] = current;
+        //Serial.print(current);  
         count = 0;
     }
+
+    if(bcount == 7){
+      trans(bits);
+      bcount = 0;
+      bits = {};
+    }
+    /******mychanges***********/
 }
