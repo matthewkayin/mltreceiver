775746087c9e80258281abfe5cb28871b74c8c03
Angel O. Lopez <32178007+AngelLopez8@users.noreply.github.com>
2019-12-05 20:08:13 -0600

    Please enter the commit message for your changes. Lines starting

--- MLT-receiver/MLT-reciever2.0.ino
+++ MLT-receiver/MLT-reciever2.0.ino
@@ -22,80 +22,86 @@ int count = 0;
 Pixy2 pixy;
 
 int trans(bool bits[]){
-	int num = 0;
-	int base = 2;
-	for(){
-		num = num + (bits[i] * pow(base, (7-1)));
-	}
-	return num;
+  int num = 0;
+  int base = 2;
+  for(){
+    num = num + (bits[i] * pow(base, (7-1)));
+  }
+  return num;
 }
 
 void steup(){
-	Serial.begin(9600);
-	Serial.print("Starting...\n");
-	
-	pix.init();
-	before_time = millis();
+  Serial.begin(9600);
+  Serial.print("Starting...\n");
+  
+  pix.init();
+  before_time = millis();
 }
 
 void loop(){
-	uinit16_t blocks;
-	int now = -1;
-	elapsed = millis() - before_time();
-	blocks = pixy.ccc.getBlocks();
-	
-	if(pixy.ccc.blocks[0].m_signature == red_sig){
-		if(current != 0){
-			current = 0;
-			count = 0;
-			recieve = true;
-		}
-		count++;
-	}else if(pixy.ccc.blocks[0].m_signature == green_sig){
-		if(current != 1){
-			current = 1;
-			count = 0;
-			recieve = true;
-		}
-		count++;
-	}else{
-		current = -1;
-		count = 0;
-	}
-	
-	if(count >= 60){
-		bits[bcount] = current;
-		bcount++;
-		count = 0;
-	}
-	
-	if(elapsed >= timeout){
-		int bitcount = (8*out_size) + bcount;
-		chunk_size = bitcount/7;
-		if{bcount !=0){
-			for(/*remaining bits*/){
-				bits[i] = 0;
-			}
-			out_buffer[out_size] = /*???/?*/;
-			out_size++;
-		}
-		ready = true;
-	}
-	if(recieve){
-		before_time = millis();
-		ready = true;
-		out_size = 0;
-		chunk_size = 0;
-		ready = false;
-		
-		if(bcount == 7){
-			bcount = 0;
-			out_buffer[out_size] = trans(bits);
-			out_size++;
-		}
-	}
-	if(ready){
-		Serial.print(out_size, chunk_size, out_buffer);
-		recieve = false;
-	}
+  uinit16_t blocks;
+  int now = -1;
+  elapsed = millis() - before_time();
+  blocks = pixy.ccc.getBlocks();
+  
+  if(pixy.ccc.blocks[0].m_signature == red_sig){
+    if(current != 0){
+      current = 0;
+      count = 0;
+      recieve = true;
+    }
+    count++;
+  }else if(pixy.ccc.blocks[0].m_signature == green_sig){
+    if(current != 1){
+      current = 1;
+      count = 0;
+      recieve = true;
+    }
+    count++;
+  }else{
+    current = -1;
+    count = 0;
+  }
+  
+  if(count >= 60){
+    bits[bcount] = current;
+    bcount++;
+    count = 0;
+  }
+  
+  if(elapsed >= timeout){
+    int bitcount = (8*out_size) + bcount;
+    chunk_size = bitcount/7;
+    if{bcount !=0){
+      for(int i = 0; i < 7;i++){
+        if(bits[i] == 0 || bits[i] == 1){
+        {
+          //do nothing
+        }else{
+          bits[i] = 0;
+        }
+        }
+      }
+      out_buffer[out_size] = trans(bits);
+      out_size++;
+    }
+    ready = true;
+  }
+  if(recieve){
+    before_time = millis();
+    ready = true;
+    out_size = 0;
+    chunk_size = 0;
+    ready = false;
+    
+    if(bcount == 7){
+      bcount = 0;
+      out_buffer[out_size] = trans(bits);
+      out_size++;
+    }
+  }
+  if(ready){
+    Serial.print(out_size, chunk_size, out_buffer);
+    recieve = false;
+  }
 }
